7c59f77bcae501de7fb1b08b48f7d2294469f0e7
Mahesh Peddi <maheshgithub@gmail.com>
2020-06-18 20:12:44 +0100

    introduced product service

---
+++ README.md
@@ -0,0 +1,76 @@
+NodeJS commands
+---------------
+
+npm init -y
+
+npm install express body-parser cors mongoose swagger-ui-express superagent
+
+
+Launching nodejs app from command prompt
+----------------------------------------
+
+sudo npm install -g nodemon
+
+nodemon server.js
+
+
+Launching nodejs app from command prompt with environment variables
+-------------------------------------------------------------------
+
+CUSTOMER_SERVICE_HOST=localhost PRODUCT_SERVICE_HOST=localhost nodemon server.js
+
+
+
+Docker Build
+------------
+
+docker build -t customer-service .
+
+docker build -t order-service .
+
+docker build -t product-service .
+
+
+Local (docker) execution:
+-------------------------
+
+docker run -p 9090:9090 customer-service
+
+docker run -p 9091:9091 product-service
+
+docker run -p 9000:9000 -e CUSTOMER_SERVICE_HOST=localhost -e PRODUCT_SERVICE_HOST=localhost order-service
+
+
+Docker-compose execution:
+-------------------------
+
+docker-compose up -d
+
+docker-compose down
+
+
+
+Container Images (GKE):
+------------------------
+
+docker tag order-service eu.gcr.io/ssil1-258911/order-service:v1
+
+docker tag customer-service eu.gcr.io/ssil1-258911/customer-service:v1
+
+docker tag product-service eu.gcr.io/ssil1-258911/product-service:v1
+
+docker push eu.gcr.io/ssil1-258911/order-service:v1
+
+docker push eu.gcr.io/ssil1-258911/customer-service:v1
+
+docker push eu.gcr.io/ssil1-258911/product-service:v1
+
+
+K8S executin (GKE):
+-------------------
+
+gcloud container clusters create --machine-type n1-standard-2 --num-nodes 2  --zone europe-west2-b --cluster-version latest test-cluster
+
+kubectl apply -f k8s-service.yml
+
+kubectl get services
--- order-service/server.js
+++ order-service/server.js
@@ -5,7 +5,7 @@ var express = require("express"),
   router = express.Router(),
   bodyParser = require("body-parser"),
   cors = require("cors"),
-  request = require("request"),
+  request = require("superagent"),
   swaggerUi = require("swagger-ui-express"),
   swaggerDocument = require("./swagger.json");
 
@@ -20,46 +20,71 @@ app.use(
 app.use(bodyParser.json());
 
 // url
-const customerService = "http://35.189.92.101:9090/api/v1/customer";
+const customerServiceHost = process.env.CUSTOMER_SERVICE_HOST || "localhost";
+const customerService =
+  "http://" + customerServiceHost + ":9090/api/v1/customer/";
+
+const productServiceHost = process.env.PRODUCT_SERVICE_HOST || "localhost";
+const productService = "http://" + productServiceHost + ":9091/api/v1/product/";
 
 // methods
-var getAllCustomers = function (req, res) {
-  request.get(
-    {
-      url: `${customerService}`,
-    },
-    (err, customerResponse, body) => {
-      if (!err) {
-        res.status(200).send(body);
-      } else {
-        res
-          .status(400)
-          .send({ problem: `Customer service responded with issue ${err}` });
-      }
-    }
-  );
+var getAllCustomers = function (req, res, next) {
+  request
+    .get(customerService)
+    .then((resp) => {
+      res.status(200).send(resp.body);
+    })
+    .catch((err) => {
+      res
+        .status(400)
+        .send({ problem: `Customer service responded with issue ${err}` });
+    });
 };
 
-var getCustomerById = function (req, res) {
-  request.get(
-    {
-      url: `${customerService}/${req.params.id}`,
-    },
-    (err, customerResponse, body) => {
-      if (!err) {
-        res.status(200).send(body);
-      } else {
-        res
-          .status(400)
-          .send({ problem: `Customer service responded with issue ${err}` });
-      }
-    }
-  );
+var getCustomerById = function (req, res, next) {
+  request
+    .get(customerService + req.params.id)
+    .then((resp) => {
+      res.status(200).send(resp.body);
+    })
+    .catch((err) => {
+      res
+        .status(400)
+        .send({ problem: `Customer service responded with issue ${err}` });
+    });
+};
+
+var getAllProducts = function (req, res, next) {
+  request
+    .get(productService)
+    .then((resp) => {
+      res.status(200).send(resp.body);
+    })
+    .catch((err) => {
+      res
+        .status(400)
+        .send({ problem: `Product service responded with issue ${err}` });
+    });
+};
+
+var getProductById = function (req, res, next) {
+  request
+    .get(productService + req.params.id)
+    .then((resp) => {
+      res.status(200).send(resp.body);
+    })
+    .catch((err) => {
+      res
+        .status(400)
+        .send({ problem: `Product service responded with issue ${err}` });
+    });
 };
 
 // route mapping
-router.route("/").get(getAllCustomers);
-router.route("/:id").get(getCustomerById);
+router.route("/customer").get(getAllCustomers);
+router.route("/customer/:id").get(getCustomerById);
+router.route("/product").get(getAllProducts);
+router.route("/product/:id").get(getProductById);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/v1/order", router);
 
